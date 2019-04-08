4ee057389f327e79c957b2f3714b000d98eb99e6
Mark Blue <mblue@students.mchenry.edu>
2019-04-07 20:10:29 -0500

    added charity screen layout added code for mainactivty added code for charity

--- app/src/main/java/com/example/charitable/MainActivity.java
+++ app/src/main/java/com/example/charitable/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.charitable;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnDonate = findViewById(R.id.btnDonate);
+        // TODO: Include see Donation Amount
+
+        btnDonate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Charity.class));
+            }
+        });
     }
 }
